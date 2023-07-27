313806389e232f572da781d13e0e3940d88db4ec
olo02 <121186383+olo02@users.noreply.github.com>
2023-07-27 21:19:00 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        
+        String result = my_string.replace(letter, "");
+        
+        return result;
+    }
+}
