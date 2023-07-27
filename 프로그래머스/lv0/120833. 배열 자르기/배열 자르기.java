b6a93e3e2ecb57781405b0812a6c447dfe15c489
olo02 <121186383+olo02@users.noreply.github.com>
2023-07-27 21:17:30 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        
+        int[] result = new int[num2 - num1 + 1];
+        
+        for(int i = num1 ; i <= num2 ; i++) {
+            result[i - num1] = numbers[i];
+        }
+        
+        return result;
+    }
+}
