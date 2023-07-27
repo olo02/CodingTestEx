4632c7ce38a96c64a2af994c789e1f3a6e5079a9
olo02 <121186383+olo02@users.noreply.github.com>
2023-07-27 21:25:47 +0900

    [level 0] Title: A로 B 만들기, Time: 2.50 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(String before, String after) {
+        return Arrays.stream(after.split("")).sorted().collect(Collectors.joining())
+            .equals(Arrays.stream(before.split("")).sorted().collect(Collectors.joining())) ? 1 : 0 ;
+    }
+}
