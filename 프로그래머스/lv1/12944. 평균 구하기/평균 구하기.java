1a33a374b91cddc13c03e8f7fe1c7c2b67bcde56
olo02 <121186383+olo02@users.noreply.github.com>
2023-07-30 01:17:23 +0900

    [level 1] Title: 평균 구하기, Time: 2.08 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -2,6 +2,6 @@ import java.util.stream.IntStream;
 
 class Solution {
     public double solution(int[] arr) {
-        return IntStream.of(arr).sum() / (double) arr.length;
+        return IntStream.of(arr).average().getAsDouble();
     }
 }
