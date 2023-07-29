e072dcd62a571de4da5d7962dc767b2d0e12a5ab
olo02 <121186383+olo02@users.noreply.github.com>
2023-07-30 06:59:16 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 2.36 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,10 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int left, int right) {
+        return IntStream.rangeClosed(left, right)
+            .map(i -> (IntStream.rangeClosed(1, i/2)
+                       .filter(j -> i % j == 0).count() + 1) % 2 == 0 ? i : -i)
+            .sum();
+    }
+}
